b1d1911ab1009d871c54d779ade4da551dff9cc9
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2019-11-26 08:10:51 +0000

    Updated docs

--- doc/auto/api.el
+++ doc/auto/api.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,28 +31,30 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgaa3e908"
-    "sec:org6b7eb8c"
-    "sec:org649a170"
-    "sec:org0bfd702"
-    "sec:org9f2bd78"
-    "sec:org38b7ee0"
-    "sec:org3445e95"
-    "sec:org4251e91"
-    "sec:org92f6d09"
-    "sec:orgda8113a"
-    "sec:orgb9ff69c"
-    "sec:orgeb7ee58"
-    "sec:org423af3d"
-    "sec:org18b287f"
-    "sec:org2081c90"
-    "sec:orgab97dea"
-    "sec:orgfec1eb9"
-    "sec:org2118e74"
-    "sec:orga08bd35"
-    "sec:orgdaca863"
-    "sec:org5ba6b08"
-    "sec:org0380756"
-    "sec:org1103382"))
+    "sec:orgf1d93eb"
+    "sec:org3a1d720"
+    "sec:orgbf32299"
+    "sec:org8852a24"
+    "sec:org123a97a"
+    "sec:org477a42b"
+    "sec:org62ed299"
+    "sec:org5c2d377"
+    "sec:org1e08338"
+    "sec:org08a734b"
+    "sec:org207bd47"
+    "sec:orgbcf20a3"
+    "sec:org39cd272"
+    "sec:org763c23c"
+    "sec:org682b186"
+    "sec:org979a01e"
+    "sec:orgfb92061"
+    "sec:org7249e48"
+    "sec:org7650fcb"
+    "sec:orga3d67dc"
+    "sec:orgf4739aa"
+    "sec:orgcb51652"
+    "sec:orgdb45025"
+    "sec:org66ca10e"
+    "sec:org959579a"))
  :latex)
 
