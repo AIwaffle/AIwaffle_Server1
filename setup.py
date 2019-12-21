4cf0a74153c60674752399404f31136a4b3ca110
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2019-12-21 16:39:12 +0800

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='server1',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
