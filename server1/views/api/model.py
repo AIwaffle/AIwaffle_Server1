b01469be48f6b62e683b85dddd313926f0342d9f
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2020-02-06 12:24:33 +0800

    Make model api compatible with form data

--- server1/views/api/model.py
+++ server1/views/api/model.py
@@ -30,8 +30,12 @@ def iter_():
 
     TODO: Complete docs for model
     """
-    session_id = flask.request.json.get("session_id", None)
-    learning_rate = flask.request.json.get("learning_rate", 0.01)
+    if flask.request.json is None:
+        session_id = flask.request.form.get("session_id", None)
+        learning_rate = flask.request.form.get("learning_rate", 0.01)
+    else:
+        session_id = flask.request.json.get("session_id", None)
+        learning_rate = flask.request.json.get("learning_rate", 0.01)
     if session_id is None:
         flask.abort(400)
     return server1.api.model.model("iterate", sid=session_id, learning_rate=learning_rate)
