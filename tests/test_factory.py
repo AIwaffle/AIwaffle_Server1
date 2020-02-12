ad9913568419b21c952dedb5cb31d3249d0ee4fa
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2020-02-12 21:18:24 +0800

    Fixed testing issues

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,10 +4,10 @@ import server1
 
 
 def test_config():
-    assert not server1.create_app().testing
+    assert not server1.create_app(dict()).testing
     assert server1.create_app({"TESTING": True}).testing
 
 
 def test_test(client):
     response = client.get("/test")
-    assert json.loads(response.data).get("success", False) == True
+    assert json.loads(response.data).get("success", False)
